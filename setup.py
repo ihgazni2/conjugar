870b811b91d4f976da4fcab216d4a1979ca5512f
dli <terryinzaghi@163.com>
2018-11-22 11:37:16 +0000

    remove old build

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup, find_packages
 setup(
-      #name ="main" name = "edict"
       name="conjugar",
-      version = "0.1",
+      version = "0.2",
       description="handle....,APIs",
       author="dapeli",
-      #https://github.com/ihgazni2/main
       url="https://github.com/ihgazni2/conjugar",
       author_email='terryinzaghi@163.com', 
       license="MIT",
-      #refer to .md files in https://github.com/ihgazni2/main 
       long_description = "refer to .md files in https://github.com/ihgazni2/conjugar",
       classifiers=[
           'Environment :: Console',
@@ -20,7 +17,6 @@ setup(
           'Programming Language :: Python',
           ],
       packages= find_packages(),
-      #py_modules=['main']
       py_modules=['conjugar'], 
       )
 
